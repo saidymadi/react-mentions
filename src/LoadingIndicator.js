b916a0492abfb3fa17524f1905cc2e5961091fa8
Said Madi <saidm@surveymonkey.com>
2017-11-16 11:26:12 -0500

    fix loading indicator margin

--- src/LoadingIndicator.js
+++ src/LoadingIndicator.js
@@ -4,7 +4,7 @@ import DelayRender from './react-delay-render';
 
 function LoadingIndicator({ style }) {
   return (
-    <div className="mentions-suggestions__loading-container">
+    <div style={{margin:0}} className="mentions-suggestions__loading-container">
       <div className="mentions-suggestions__sm-spin spin-small">
       </div>
     </div>
